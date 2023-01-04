d8cc0098a3e11bd63495d3446d2440fee92f5c14
RDI <dkdlsekdls@naver.com>
2023-01-04 16:06:54 +0900

    feat/useEffect를 이용하여 useState 사용시에만 API호출

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
     console.log("CALL the api...");
   }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me </button>
     </div>
