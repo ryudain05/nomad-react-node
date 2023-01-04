f4c0bc6a944b274776eb71d6c82b8c0bb3bec53f
RDI <dkdlsekdls@naver.com>
2023-01-04 15:54:27 +0900

    feat/useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL the api...");
+  }, []);
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Cotinue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me </button>
     </div>
   );
 }
