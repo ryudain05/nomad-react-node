009ebe6b639ccc2dbb6f42ae867438d2c77f724e
RDI <dkdlsekdls@naver.com>
2023-01-04 17:01:47 +0900

    feat/useEffect byFn, hiFn 사용연습

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(");
+    };
   }, []);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'Keyword' changes.");
-    }
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me </button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
