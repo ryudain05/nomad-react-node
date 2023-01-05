44f0cbb6e3d3acd699dee44c42da2c751b519e69
RDI <dkdlsekdls@naver.com>
2023-01-05 15:13:55 +0900

    feat/gh pages오류 수정v.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       </Routes>
     </Router>
   );
