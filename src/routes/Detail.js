28c22333d6232a1568c137021e7a7a9b30ae0ef2
RDI <dkdlsekdls@naver.com>
2023-01-08 17:28:49 +0900

    feat/detail 페이지 링크 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,17 +4,14 @@ import Movie from "../components/Movie";
 import styles from "./Home.module.css";
 
 function Detail() {
-  const [loading, setLoading] = useState(true);
+  const [movies, setMovie] = useState([]);
   const { id } = useParams();
-  const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
+      await fetch(`https://yts.mx/api/v2/movie_details.json?&movie_id=${id}`)
     ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
+    console.log(json);
+    setMovie(json.data);
   };
   useEffect(() => {
     getMovies();
@@ -22,23 +19,9 @@ function Detail() {
   console.log(id);
   return (
     <div>
-      {loading ? (
-        <h1 className={styles.movies}>Loading...</h1>
-      ) : (
-        <div className={styles.movies}>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-              year={movie.year}
-            />
-          ))}
-        </div>
-      )}
+      <div className={styles.movies}>
+        <h1>{movies.title}</h1>
+      </div>
     </div>
   );
 }
